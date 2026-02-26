d50009e53a16b05506557df590d5f469a5df2311
Arnab Biswas <arnabbiswas034@gmail.com>
2026-02-26 17:58:21 +0530

    cors

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const port = process.env.PORT || 5000;
 const campaignRouter = require('./routes/campaignRouter')
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
